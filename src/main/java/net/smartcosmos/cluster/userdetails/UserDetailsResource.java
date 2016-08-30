3d4f962408a552dbe477611452ba6f4d9b15af67
Will Schneider <will.schneider@smartrac-group.com>
2016-08-30 16:10:03 -0400

    read user details from properties

--- src/main/java/net/smartcosmos/cluster/userdetails/UserDetailsResource.java
+++ src/main/java/net/smartcosmos/cluster/userdetails/UserDetailsResource.java
@@ -1,9 +1,6 @@
 package net.smartcosmos.cluster.userdetails;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import javax.validation.Valid;
 
 import lombok.extern.slf4j.Slf4j;
@@ -19,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.smartcosmos.cluster.userdetails.domain.AuthenticateRequest;
+import net.smartcosmos.cluster.userdetails.domain.UserAuthenticationProperties;
 import net.smartcosmos.cluster.userdetails.domain.UserDto;
 
 @RestController
@@ -26,8 +24,13 @@ import net.smartcosmos.cluster.userdetails.domain.UserDto;
 public class UserDetailsResource {
 
     @Autowired
-    PasswordEncoder passwordEncoder;
+    private PasswordEncoder passwordEncoder;
 
+    private UserAuthenticationProperties userAuthenticationProperties;
+    @Autowired
+    public UserDetailsResource(UserAuthenticationProperties userAuthenticationProperties) {
+        this.userAuthenticationProperties = userAuthenticationProperties;
+    }
     @RequestMapping(value = "authenticate", method = RequestMethod.POST)
     public UserDto authenticate(@RequestBody @Valid AuthenticateRequest authentication)
         throws UsernameNotFoundException, IOException {
@@ -36,71 +39,24 @@ public class UserDetailsResource {
                  authentication);
 
         String passwordHash = null;
-        if (StringUtils.isNotBlank(authentication.getCredentials())) {
-            String credentials = authentication.getCredentials();
-            if (!"password".equals(credentials)) {
+        if (StringUtils.equals(userAuthenticationProperties.getName(), authentication.getPrincipal())) {
+            if (!StringUtils.equals(userAuthenticationProperties.getPassword(), authentication.getCredentials())) {
                 log.error("Password incorrect.");
                 throw new BadCredentialsException("Invalid password");
             }
-            passwordHash = passwordEncoder.encode(credentials);
+            passwordHash = passwordEncoder.encode(authentication.getCredentials());
         }
 
-        final String accountUrn = "urn:account:uuid:53f452c2-5a01-44fd-9956-3ecff7c32b30";
+        final String tenantUrn = "urn:account:uuid:53f452c2-5a01-44fd-9956-3ecff7c32b30";
         final String userUrn = "urn:user:uuid:53f452c2-5a01-44fd-9956-3ecff7c32b30";
 
-        return UserDto.builder().tenantUrn(accountUrn).userUrn(userUrn)
-            .username(authentication.getName()).passwordHash(passwordHash)
-            .authorities(getAuthorities())
+        return UserDto.builder()
+            .tenantUrn(tenantUrn)
+            .userUrn(userUrn)
+            .username(authentication.getName())
+            .passwordHash(passwordHash)
+            .authorities(userAuthenticationProperties.getAuthorities())
             .build();
     }
 
-    /**
-     * Return all available authorities
-     *
-     * @return List of authorities
-     */
-    private List<String> getAuthorities() {
-
-        List<String> authorities = new ArrayList<>();
-
-        // Things
-        String[] thingsAuthorities = {
-            "https://authorities.smartcosmos.net/things/create",
-            "https://authorities.smartcosmos.net/things/read",
-            "https://authorities.smartcosmos.net/things/update",
-            "https://authorities.smartcosmos.net/things/delete" };
-        authorities.addAll(Arrays.asList(thingsAuthorities));
-
-        // Metadata
-        String[] metadataAuthorities = {
-            "https://authorities.smartcosmos.net/metadata/create",
-            "https://authorities.smartcosmos.net/metadata/read",
-            "https://authorities.smartcosmos.net/metadata/update",
-            "https://authorities.smartcosmos.net/metadata/delete" };
-        authorities.addAll(Arrays.asList(metadataAuthorities));
-
-        // Relationships
-        String[] relationshipAuthorities = {
-            "https://authorities.smartcosmos.net/relationships/create",
-            "https://authorities.smartcosmos.net/relationships/read",
-            "https://authorities.smartcosmos.net/relationships/delete" };
-        authorities.addAll(Arrays.asList(relationshipAuthorities));
-
-        String[] profilesAuthorities = {
-            "https://profiles-authorities.smartcosmos.net/authentication/otp",
-            "https://profiles-authorities.smartcosmos.net/authentication/signature",
-            "https://profiles-authorities.smartcosmos.net/tag/properties/read",
-            "https://profiles-authorities.smartcosmos.net/tag/properties/definition",
-            "https://profiles-authorities.smartcosmos.net/query/batches",
-            "https://profiles-authorities.smartcosmos.net/query/tags",
-            "https://profiles-authorities.smartcosmos.net/tag/value/read",
-            "https://profiles-authorities.smartcosmos.net/tag/value/update",
-            "https://profiles-authorities.smartcosmos.net/tag/key/read",
-            "https://profiles-authorities.smartcosmos.net/tig/read"
-        };
-
-        authorities.addAll(Arrays.asList(profilesAuthorities));
-
-        return authorities;
-    }
 }
--- src/main/java/net/smartcosmos/cluster/userdetails/UserDetailsService.java
+++ src/main/java/net/smartcosmos/cluster/userdetails/UserDetailsService.java
@@ -4,16 +4,19 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 
+import net.smartcosmos.cluster.userdetails.domain.UserAuthenticationProperties;
 @SpringBootApplication
 @Controller
 @EnableDiscoveryClient
 @Slf4j
+@EnableConfigurationProperties({ UserAuthenticationProperties.class})
 public class UserDetailsService {
 
     public static void main(String[] args) {
