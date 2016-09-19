2ef1a19dc4753cb7119be55a470aa2c2b38854c2
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-09-19 12:41:16 +0200

    Use empty list as default for authorities to avoid NPE

--- src/main/java/net/smartcosmos/cluster/userdetails/domain/UserAuthenticationProperties.java
+++ src/main/java/net/smartcosmos/cluster/userdetails/domain/UserAuthenticationProperties.java
@@ -1,21 +1,22 @@
 package net.smartcosmos.cluster.userdetails.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import lombok.Data;
-
 import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Data;
 
 /**
  * Configuration properties for dummy user.
  */
 @Data
-@ConfigurationProperties("smartcosmos.security.user")
+@ConfigurationProperties("smartcosmos.user")
 public class UserAuthenticationProperties {
 
     private String name = "user";
     private String password = "password";
-    private List<String> authorities;
+    private List<String> authorities = new ArrayList<>();
     private String tenantUrn = "urn:tenant:uuid:513161AA-24B0-410F-BE1D-D6CD04B6C9BB";
     private String userUrn = "urn:user:uuid:71266402-55BC-4267-B56E-407BE9778C3B";
 }
